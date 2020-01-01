0f497c0cbd9a7ca9b4d709ebb1bd34a527386ca1
Abhishek Saral <abhisheksaral17@gmail.com>
2020-01-01 14:33:05 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Abhishek Saral on 1/1/20.
+//  Copyright © 2020 Tech Knowns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
